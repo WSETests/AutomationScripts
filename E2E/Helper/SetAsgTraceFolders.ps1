beb7e11dbee074e4cd64a985db9ce5f6ed14e524
Jayshree Dugar <jdugar@microsoft.com>
2025-01-14 12:10:44 -0800

    Deleteing the folders

--- E2E/Helper/SetAsgTraceFolders.ps1
+++
@@ -1,7 +0,0 @@
-﻿function CreateScenarioLogsFolder ($snario)
-{
-    $scenarioLogsFolder = $pathLogsFolder + "\$snario"
-    If (!(test-path $scenarioLogsFolder)) {
-        New-Item -ItemType Directory -Force -Path $scenarioLogsFolder  | Out-Null
-    }
-}
